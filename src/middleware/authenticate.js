8d583fc49be00a203e4464a828367b7d6bdd6a52
pradeep-lenka-dev <pradeeplenka6@gmail.com>
2024-04-09 21:28:53 +0530

    add refresh token

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -3,20 +3,26 @@ require("dotenv").config()
 
 function authenticate(req, res, next) {
     let header = req.headers
-    console.log("🚀 ~ authenticate ~ header:", header)
     let authorization = req.headers.authorization
-    console.log("🚀 ~ authenticate ~ authorization:", authorization)
     const token = JSON.parse(authorization);
-    console.log("🚀 ~ authenticate ~ token:", token.token.accessToken)
     const secretKey = process.env.JWT_SECRET;
     jwt.verify(token.token.accessToken, secretKey, (err, decoded) => {
         if (err) {
-            console.error("expenseController 18 err:", err)
+            //return res.status(401).json({ error: 'Unauthorized' });
+            jwt.verify(token.token.refreshToken, secretKey, (err, decoded) => {
+                if (err) {
+                    return res.status(401).json({ error: 'Unauthorized' });
+                }
+                user_id = decoded._id
+                req.user = decoded;
+                next();
+            })
+        } else {
+            user_id = decoded._id
+            req.user = decoded;
+            next();
+
         }
-        user_id = decoded._id
-        req.user = decoded;
-        next();
-        console.warn("expenseController 18 decoded:", decoded)
     })
 
 
